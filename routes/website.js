51a492a76e30cef6b88acbb8a40037f23331acc6
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2023-09-22 16:33:58 -0300

    category selector in publish page updated

--- routes/website.js
+++ routes/website.js
@@ -37,58 +37,12 @@ router.get('/perfil/:email', (req, res)=>{
     })
 })
 
-router.get('/artigos', (req, res)=>{
-    Post.find({category: 'Artigo'}).sort({_id: -1}).limit(20).lean().then((posts)=>{
-        Post.find({category: "Artigo"}).sort({_id: -1}).limit(5).then().then((post)=>{
-            res.render('website/artigos', {posts: posts, post: post})
-        })
-    })
-})
-
-router.get('/noticias', (req, res)=>{
-    Post.find({category: 'Noticia'}).sort({_id: -1}).limit(20).lean().then((posts)=>{
-        Post.find({category: "Noticia"}).sort({_id: -1}).limit(5).lean().then().then((post)=>{
-            res.render('website/noticias', {posts: posts, post: post})
-        })
-    })
-})
-
-router.get('/literatura', (req, res)=>{
-    Post.find({category: 'Literatura'}).sort({_id: -1}).limit(20).lean().then((posts)=>{
-        Post.find({category: "Literatura"}).sort({_id: -1}).limit(5).lean().then().then((post)=>{
-            res.render('website/literatura', {posts: posts, post: post})
-        })
-    })
-})
-
-router.get('/economia', (req, res)=>{
-    Post.find({category: 'Economia'}).sort({_id: -1}).limit(20).lean().then((posts)=>{
-        Post.find({category: "Economia"}).sort({_id: -1}).limit(5).lean().then().then((post)=>{
-            res.render('website/economia', {posts: posts, post: post})
-        })
-    })
-})
-
-router.get('/tecnologia', (req, res)=>{
-    Post.find({category: 'Tecnologia'}).sort({_id: -1}).limit(20).lean().then((posts)=>{
-        Post.find({category: "Tecnologia"}).sort({_id: -1}).limit(5).lean().then().then((post)=>{
-            res.render('website/tecnologia', {posts: posts, post: post})
-        })
-    })
-})
-
-router.get('/empreededorismo', (req, res)=>{
-    Post.find({category: 'Empreendedorismo'}).sort({_id: -1}).limit(20).lean().then((posts)=>{
-        Post.find({category: "Empreendedorismo"}).sort({_id: -1}).limit(5).lean().then().then((post)=>{
-            res.render('website/empreededorismo', {posts: posts, post: post})
-        })
-    })
-})
-
 router.get('/categoria/:titulo', (req, res) => {
     Post.find({category: req.params.titulo}).sort({_id: -1}).limit(30).lean().then((posts) => {
         Post.find({category: req.params.titulo}).sort({_id: -1}).limit(6).lean().then((post) => {
-            res.render('website/categorias', {posts: posts, post: post})
+            Post.find({category: req.params.titulo}).sort({_id: -1}).limit(1).lean().then((category) => {
+                res.render('website/categorias', {posts: posts, post: post, category: category})
+            })
         })
     })
 })
